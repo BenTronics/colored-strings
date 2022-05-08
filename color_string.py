dc09a9026dc7b28c016736c5a055258cdc073db1
Benni <>
2022-05-08 21:54:14 +0200

    fg implementierung

---
+++ color_string.py
@@ -0,0 +1,28 @@
+FG_COLOR=\
+{
+    "black" : "\033[30m",
+    "red" : "\033[31m",
+    "green" : "\033[32m",
+    "yellow" : "\033[33m",
+    "blue" : "\033[34m",
+    "magenta" : "\033[35m",
+    "cyan" : "\033[36m",
+    "white" : "\033[37m"
+}
+
+BG_COLOR=\
+{
+    "black" : "\033[40m",
+    "red" : "\033[41m",
+    "green" : "\033[42m",
+    "yellow" : "\033[43m",
+    "blue" : "\033[44m",
+    "magenta" : "\033[45m",
+    "cyan" : "\033[46m",
+    "white" : "\033[47m"
+}
+
+RESET = "\033[0m"
+
+def colorize_str(string, fg=FG_COLOR["white"]):
+    return FG_COLOR[fg] + string + RESET
